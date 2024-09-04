e657834801edeae24cd6bbeaa831a42067c4571a
unknown <dixitgarima24@gmail.com>
2024-09-04 15:48:00 +0530

    new

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,3 +1,44 @@
+// import { useEffect, useState } from "react";
+
+// export default function Stopwatch() {
+//   const [isRunning, setIsRunning] = useState(false);
+//   const [elapsedTime, setElapsedTime] = useState(0);
+
+//   useEffect(() => {
+//     let intervalId;
+//     if (isRunning) {
+//       intervalId = setInterval(() => {
+//         setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+//       }, 1000);
+//     } else {
+//       clearInterval(intervalId);
+//     }
+//     return () => clearInterval(intervalId);
+//   }, [isRunning]);
+
+//   const startStop = () => {
+//     setIsRunning((prevIsRunning) => !prevIsRunning);
+//   };
+//   const reset = () => {
+//     setIsRunning(false);
+//     setElapsedTime(0);
+//   };
+
+//   const formatTime = (sec) => {
+//     const min = Math.floor(sec / 60);
+//     const remainingSec = sec % 60;
+//     return `${min}:${remainingSec < 10 ? "0" : ""}${remainingSec}`;
+//   };
+//   return (
+//     <div>
+//       <h1>Stopwatch</h1>
+//       <p>Time: {formatTime(elapsedTime)}</p>
+//       <button onClick={startStop}>{isRunning ? "Stop" : "Start"}</button>
+//       <button onClick={reset}>Reset</button>
+//     </div>
+//   );
+// }
+
 import { useEffect, useState } from "react";
 
 export default function Stopwatch() {
@@ -10,8 +51,6 @@ export default function Stopwatch() {
       intervalId = setInterval(() => {
         setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
       }, 1000);
-    } else {
-      clearInterval(intervalId);
     }
     return () => clearInterval(intervalId);
   }, [isRunning]);
@@ -19,6 +58,7 @@ export default function Stopwatch() {
   const startStop = () => {
     setIsRunning((prevIsRunning) => !prevIsRunning);
   };
+
   const reset = () => {
     setIsRunning(false);
     setElapsedTime(0);
@@ -27,8 +67,9 @@ export default function Stopwatch() {
   const formatTime = (sec) => {
     const min = Math.floor(sec / 60);
     const remainingSec = sec % 60;
-    return `${min} : ${remainingSec < 10 ? "0" : ""}${remainingSec}`;
+    return `${min}:${remainingSec < 10 ? "0" : ""}${remainingSec}`;
   };
+
   return (
     <div>
       <h1>Stopwatch</h1>
