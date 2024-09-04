7ca15658d673d95920d8c6cd2591b0020dca0a38
unknown <dixitgarima24@gmail.com>
2024-09-04 12:50:58 +0530

    timer

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// import Stopwatch from './Stopwatch'
+
+function App() {
+  return (
+    <div className="App">
+     {/* <h5>Stopwatch</h5> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Stopwatch.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+export default function Stopwatch() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const startStop = () => {
+    setIsRunning((prevIsRunning) => !prevIsRunning);
+  };
+  const reset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+  };
+
+  const formatTime = (sec) => {
+    const min = Math.floor(sec / 60);
+    const remainingSec = sec % 60;
+    return `${min}: ${remainingSec < 10 ? "0" : ""}${remainingSec}`;
+  };
+  return (
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time:{formatTime(elapsedTime)}</p>
+      <button onClick={startStop}>{isRunning ? "stop" : "Start"}</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
